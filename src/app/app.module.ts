62566829b14f54ef89f35c48fe0e4877c52688fc
barackmaund1 <pmaundu49@gmail.co>
2020-04-14 22:37:44 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepoComponent } from './repo/repo.component';
+import { UserComponent } from './user/user.component';
+import { UsersearchFormComponent } from './usersearch-form/usersearch-form.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PipePipe } from './pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepoComponent,
+    UserComponent,
+    UsersearchFormComponent,
+    NotFoundComponent,
+    NavbarComponent,
+    PipePipe
   ],
   imports: [
     BrowserModule,
