5239d9dec230a36bafcf90303501513a03b319af
barackmaund1 <pmaundu49@gmail.co>
2020-04-15 09:35:53 +0300

    changed some files

--- src/app/http-request/service.service.ts
+++ src/app/http-request/service.service.ts
@@ -1,6 +1,5 @@
 import { environment } from './../../environments/environment.prod';
-import { Repo } from './../repo-class/repo';
-import { User } from './../user-class/user';
+import { Repo } from './../repo-class/repo'
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpParams } from '@angular/common/http';
 
@@ -26,7 +25,7 @@ export class ServiceService {
      created_at:Date;
     }
     return new Promise((resolve,reject)=>{
-      this.http.get<Response>('https:api.github.com/users/'+searchName+ "/repos?order=created&sort=asc?access_token=" + environment.apiKey).toPromise().then(
+      this.http.get<Response>('https:api.github.com/users/'+ searchName + "/repos?access_token=" + environment.apiKey).toPromise().then(
         (results)=>{
           this.getRepos=results;
           console.log(this.getRepos)
